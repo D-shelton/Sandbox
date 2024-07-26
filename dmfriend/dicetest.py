02d73107537f7264fce30f91a6eb3db607af8ab0
D-Shelton <djstheii@gmail.com>
2024-07-26 16:00:55 -0500

    attemp to build assignstats func for complex methods

--- dmfriend/dicetest.py
+++ dmfriend/dicetest.py
@@ -1,5 +1,5 @@
 import random
-NUM_STATS = 6
+STATS = ['str', 'dex', 'con', 'int', 'wis', 'cha']
 
 def find_method():
     method = input(
@@ -28,15 +28,30 @@ def roll_dice(num, sides):
         rolled_nums.append(roll)
     return rolled_nums
 
-def assign_value(rolled_stats):
-    for i in range(0, len(rolled_stats)):
-        pass
+def assign_stats(rolled_stats):
+    final_stats = []
+    used_stats = []
+    print(f"Your available stat choices are {rolled_stats}\n")
+    while len(rolled_stats) != 0:
+        currentStat = input("Which stat would you like to assign? ")
+        if currentStat.lower() in STATS:
+            used_stats.append(currentStat)
+            pickedStat = input("Which roll would you like to apply?")
+            if pickedStat in rolled_stats:
+                pass
+                #apply the stat to its location and send it
+
+    #this is a temp plan, maybe use while stats in rolled_stats instead
+    #ask "which stat would you like to update"
+    #assign stat to that index place (make list of indexed stats?)
+    #output should be an ordered stat block in order of STR DEX CON INT WIS CHA
+    #output is currently a list, change to dict or rely on list order?
 
 def roll_straight():
     #rolls stats straight in a row, D&D1e method
     statblock = []
     fullroll = []
-    for i in range(NUM_STATS):
+    for i in range(len(STATS)):
         dice_roll = roll_dice(3, 6)
         total_roll = sum(dice_roll)
         statblock.append(total_roll)
@@ -48,7 +63,7 @@ def roll_4d6():
     #modern roll 4d6 drop 1 method
     statblock = []
     fullroll = []
-    for i in range(NUM_STATS):
+    for i in range(len(STATS)):
     #rolls stats equal to NUM_STATS
         dice_roll = roll_dice(4, 6)
         lowest = 21
@@ -63,6 +78,8 @@ def roll_4d6():
         statblock.append(sum(dice_roll))
         #single list with number of rolls equal to NUM_STATS
     print(f"Your rolls were {fullroll}")
+    statblock = assign_stats(statblock)
+    #assign_stats(statblock) will take the statblock and let the user assign values to stats
     return statblock
 
 
@@ -70,7 +87,7 @@ def roll_mighty3d6():
     #Roll 2d6+6 for stats, statblock is list of stats, fullroll is individual dice rolls
     statblock = []
     fullroll = []
-    for i in range(NUM_STATS):
+    for i in range(len(STATS)):
         dice_roll = roll_dice(2, 6)
         dice_roll.append(6)
         fullroll.append(dice_roll)
